3df3e42c1e37cd4e4cdee0ea0736680977b28f71
Max Kipka <max.kipka@icloud.com>
2026-04-01 23:25:30 +0200

    added skill

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './layout/header/header';
 import { Hero } from './layout/components/hero/hero';
 import { AboutMe } from './layout/components/about-me/about-me';
+import { Skill } from './layout/components/skill/skill';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, AboutMe],
+  imports: [Header, Hero, AboutMe, Skill],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/layout/components/skill/skill.ts
+++ src/app/layout/components/skill/skill.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './skill.html',
   styleUrl: './skill.scss',
 })
-export class Skill {}
+export class Skill {
+  protected readonly skills = [
+    { name: 'HTML', icon: 'assets/icons/html.png' },
+    { name: 'CSS', icon: 'assets/icons/css.png' },
+    { name: 'JavaScript', icon: 'assets/icons/js.png' },
+    { name: 'Material Design', icon: 'assets/icons/md.png' },
+    { name: 'TypeScript', icon: 'assets/icons/ts.png' },
+    { name: 'Angular', icon: 'assets/icons/ng.png' },
+    { name: 'Supabase', icon: 'assets/icons/sb.png' },
+    { name: 'Git', icon: 'assets/icons/git.png' },
+    { name: 'REST-API', icon: 'assets/icons/rest-api.png' },
+    { name: 'Scrum', icon: 'assets/icons/scrum.png' },
+    { name: 'Growth mindset', icon: 'assets/icons/gm.png' },
+  ];
+}
