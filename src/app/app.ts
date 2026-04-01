0da2ff9604b3bf83056c415794069c133c64e4f1
Max Kipka <max.kipka@icloud.com>
2026-04-01 19:23:15 +0200

    added about

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './layout/header/header';
 import { Hero } from './layout/components/hero/hero';
+import { AboutMe } from './layout/components/about-me/about-me';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero],
+  imports: [Header, Hero, AboutMe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
